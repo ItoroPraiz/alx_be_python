3416430c76dd83f17fb03e8d0c0e5a3379fd2c57
Joy Humphrey <humphreyjoy70@gmail.com>
2025-05-25 22:13:28 +0100

    Re-add python_introduction folder as regular directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+# Addition
+result = number1 + number2
+print("Addition of 10 and 5 is", result)
+# Subtraction
+result = number1 - number2
+print("Subtraction of 10 and 5 is", result)
+# Multiplication
+result = number1 * number2
+print("Multiplication of 10 and 5 is", result)
+# Division
+result = number1 / number2
+print("Division of 10 and 5 is", result)
