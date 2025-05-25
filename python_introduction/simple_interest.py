e6b3bb55645999a981f296d7e2e5f82576dbe14d
Joy Humphrey <humphreyjoy70@gmail.com>
2025-05-25 22:26:17 +0100

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,5 +2,6 @@ principal = 1000
 rate = 0.05
 time = 3
 # Interest
-interest = principal * rate * 3
+interest = principal * rate * time
 print("The simple interest is:", interest)
+# The simple interest is: 150.0
