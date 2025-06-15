b6d78aa82602a5ef344b0edbfe7c0153920f50a9
Joy Humphrey <humphreyjoy70@gmail.com>
2025-06-15 20:38:11 +0100

    add library_management.py file

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,4 +1,4 @@
-import sys
+'''import sys
 from robust_division_calculator import safe_divide
 
 def main():
@@ -11,6 +11,28 @@ def main():
 
     result = safe_divide(numerator, denominator)
     print(result)
+
+if __name__ == "__main__":
+    main()'''
+
+
+from library_management import Book, Library
+
+def main():
+    library = Library()
+    library.add_book(Book("Brave New World", "Aldous Huxley"))
+    library.add_book(Book("1984", "George Orwell"))
+
+    print("Available books after setup:")
+    library.list_available_books()
+
+    library.check_out_book("1984")
+    print("\nAvailable books after checking out '1984':")
+    library.list_available_books()
+
+    library.return_book("1984")
+    print("\nAvailable books after returning '1984':")
+    library.list_available_books()
 
 if __name__ == "__main__":
     main()
